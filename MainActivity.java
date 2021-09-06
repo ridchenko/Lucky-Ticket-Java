fe270d65136ce9fcc9e7e5dad01b47e3a181c8cb
ridchenko <88441052+ridchenko@users.noreply.github.com>
2021-09-06 15:57:34 +0300

    Edits 01

---
+++ MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.luckyticketjava;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public void CheckTicket(View view) {
+        EditText num = findViewById(R.id.etTicketNumber);
+        String input = num.getText().toString();
+        ImageView picture = findViewById(R.id.imageView);
+        Drawable pictureGreen = getDrawable(getResources()
+                .getIdentifier("@drawable/green_light", null, getPackageName()));
+        Drawable pictureRed = getDrawable(getResources()
+                .getIdentifier("@drawable/red_light", null, getPackageName()));
+
+        if (input.length() == 6) {
+                String[] str = Arrays.copyOfRange(input.split(""),
+                        1,
+                        input.split("").length);
+                int[] result = Arrays.stream(str).mapToInt(Integer::parseInt).toArray();
+                if (result[0] + result[1] + result[2] == result[3] + result[4] + result[5]) {
+                    picture.setImageDrawable(pictureGreen);
+                } else {
+                    picture.setImageDrawable(pictureRed);
+                }
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Enter 6 digits",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
